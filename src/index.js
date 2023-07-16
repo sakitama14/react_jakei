13403a17caffd7af1c4e575ef4be00709eae04fc
sakitama14 <104491362+sakitama14@users.noreply.github.com>
2023-07-16 15:54:24 +0900

    const, let等の変数宣言

--- src/index.js
+++ src/index.js
@@ -1,10 +1,46 @@
-import "./styles.css";
+// varを使った場合
+// var var1 = "var宣言";
+// console.log(var1);
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!！！</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// var1 = "var1を上書き";
+// console.log(var1);
+
+// var var1 = "varの再宣言";
+// console.log(var1);
+
+// letを使った場合
+// let var2 = "var宣言";
+// console.log(var2);
+
+// var2 = "var2の上書き";
+// console.log(var2);
+
+// let var2 = "varの再宣言";
+// console.log(var2);
+
+// constを使った場合
+// const var3 = "var宣言";
+// console.log(var3);
+
+// var3 = "varの上書き";
+// console.log(var2);
+
+// const var3 = "varの再宣言";
+// console.log(var2);
+
+// オブジェクトの場合は変更ができる
+// const var4 = {
+//   name: "yudai",
+//   age: 24
+// };
+// var4.name = "YUDAI";
+// console.log(var4);
+
+// 配列の場合は変更ができる
+// const var5 = ["dog", "cat"];
+// console.log(var5);
+
+// var5[0] = "bird";
+// var5.push("monkey");
+// console.log(var5);
+// 上記のような場合でもログされる配列はbird,cat,monkeyの3つになる（なぜ１個目のログは二つではない？）
